5abcc4401b9c0b1a9a1bb023fc21a6ea6b8e7115
pizzalord22 <31384712+pizzalord22@users.noreply.github.com>
2017-08-28 22:50:43 +0200

    Add files via upload

--- pizzaScript.lua
+++ pizzaScript.lua
@@ -18,20 +18,23 @@
 
 -- create vars
 pizza = {
-    CreateClientConVar( "Bhop", "1", true, false ),
-    CreateClientConVar( "esp", "1", true, false ),
-    CreateClientConVar( "aimbot", "1", true, false ),
-    CreateClientConVar( "spambot", "1", true, false ),
-    CreateClientConVar( "message", "spam message", true, false )
+  CreateClientConVar( "Bhop", "1", true, false ),
+  CreateClientConVar( "esp", "1", true, false ),
+  CreateClientConVar( "aimbot", "1", true, false ),
+  CreateClientConVar( "spambot", "1", true, false ),
+  CreateClientConVar( "message", "test", true, false )
 }
-print( GetConVarInt( "Bhop" ) )
+print( "bhop: " .. GetConVarNumber( "Bhop" ) )
+print( "esp: " .. GetConVarNumber( "esp" ) )
+print( "aimbot: " .. GetConVarNumber( "aimbot" ) )
+print( "spambot: " .. GetConVarNumber( "spambot" ) )
+print( "message: " .. GetConVarString( "message" ) )
 
 --- create a text when the hack is loaded
 function main()
-    chat.AddText( Color( 0, 0, 0 ), "[", Color( 30, 220, 0 ), " pizza v0.01 started ", Color( 0, 0, 0 ), "]" )
-    chat.AddText( Color( 0, 0, 0 ), "[", Color( 30, 220, 0 ), " have fun =) ", Color( 0, 0, 0 ), "]" )
+  chat.AddText( Color( 0, 0, 0 ), "[", Color( 30, 220, 0 ), " pizza v0.02 started ", Color( 0, 0, 0 ), "]" )
+  chat.AddText( Color( 0, 0, 0 ), "[", Color( 30, 220, 0 ), " have fun =) ", Color( 0, 0, 0 ), "]" )
 end
-
 
 --- create a menu command and a menu
 concommand.Add ("pizza_menu", function()
@@ -42,7 +45,7 @@ concommand.Add ("pizza_menu", function()
 
     --- create a Frame for the menu
     local Frame = vgui.Create( "DFrame" )
-    Frame:SetTitle( "Pizza V0.01    user: " .. LocalPlayer():Nick() )
+    Frame:SetTitle( "Pizza V0.02    user: " .. LocalPlayer():Nick() )
     Frame:SetVisible( true )
     Frame:SetDraggable( true )
     Frame:ShowCloseButton( true )
@@ -52,44 +55,47 @@ concommand.Add ("pizza_menu", function()
 
     --- create bunny hop button
     local BhopButton = vgui.Create( "DButton", Frame )
-    if pizza.Bhop == "0" then
-        BhopButton:SetText( "Bhop OFF" )
+    if GetConVarNumber( "Bhop" ) == 0 then
+        BhopButton:SetText( "Bhop OFF", "MyFont")
         BhopButton.Paint = function( self, w, h )
             draw.RoundedBox( 0, 0, 0, w, h, offColor ) -- Draw a blue button
         end
-    elseif pizza.Bhop == "1" then
+    elseif GetConVarNumber( "Bhop" ) == 1 then
         BhopButton:SetText( "Bhop ON" )
         BhopButton.Paint = function( self, w, h )
             draw.RoundedBox( 0, 0, 0, w, h, onColor ) -- Draw a green button
         end
     end
     BhopButton:SetTextColor( textColor )
+    BhopButton:SetFont( "Trebuchet20" )
     BhopButton:SetPos( 5, 30 )
     BhopButton:SetSize( 100, 30 )
 
     --- create an action to do when the Bhop button is pressed
-    Bhop.DoClick = function()
-        if pizza.Bhop == 1 then
+    BhopButton.DoClick = function()
+        if GetConVarNumber( "Bhop" ) == 0 then
+            BhopButton:SetText( "Bhop ON" )
             BhopButton.Paint = function( self, w, h )
                 draw.RoundedBox( 0, 0, 0, w, h, onColor ) -- Draw a green button
             end
-            pizza.Bhop = 0
-        else
+            RunConsoleCommand("Bhop", "1")
+        elseif GetConVarNumber( "Bhop" ) == 1 then
+            BhopButton:SetText( "Bhop OFF" )
             BhopButton.Paint = function( self, w, h )
                 draw.RoundedBox( 0, 0, 0, w, h, offColor ) -- Draw a blue button
             end
-            pizza.Bhop = 1
+            RunConsoleCommand("Bhop", "0")
         end
     end
 end)
 
 --- create bhop function
 local function Bhop()
-    if pizza.Bhop == 1 then
+    if GetConVarNumber( "Bhop" ) == 1 then
         if input.IsKeyDown(KEY_SPACE) then
             if LocalPlayer():OnGround() then
                 RunConsoleCommand("+jump")
-                timer.Create("Bhop", 0, 0.1, function()
+                timer.Create("Bhop", 0, 1, function()
                     RunConsoleCommand("-jump")
                 end )
             end
@@ -104,12 +110,16 @@ end
 
 --- create spambot function
 local function spambot()
-
+    if GetConVarNumber( "spambot" ) == 1 then
+        RunConsoleCommand( "say", GetConVarString( "message" ) )
+    end
 end
 
 --- add the hooks that should always be on to avoid troubles
 --- might need to be changed to add/remove hooks on button presses
 --- if it takes to much cpu
-hook.add( "Think", "Bhop", Bhop )
-hook.add( "Think", "aimbot", aimbot )
-hok.add ( "Think", "spambot", spambot )
+hook.Add( "Think", "BunnyHop", Bhop )
+hook.Add( "Think", "aimbot", aimbot )
+hook.Add( "Think", "spambot", spambot )
+
+main()
